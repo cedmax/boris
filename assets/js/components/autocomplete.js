b57fa267382ec3d41241b5fcbc34faaf7ed21086
Marco Cedaro <cedmax@cedmachine.local>
2016-03-24 13:58:46 +0000

    fix autocomplete styles

--- assets/js/components/autocomplete.js
+++ assets/js/components/autocomplete.js
@@ -6,12 +6,19 @@ export default class BorisAutoComplete extends React.Component {
   render() {
     var placeHolder = this.props.data[ 0 ];
     return (
-      <AutoComplete
-        id={this.props.id}
-        hintText={this.props.value ? this.props.value : `Prova con ${placeHolder}`}
-        dataSource={this.props.data}
-        filter={AutoComplete.caseInsensitiveFilter}
-        onNewRequest={this.props.onSelect} />
+      <div style={{
+          minWidth:'250px',
+          width: '50%',
+          margin: 'auto'
+      }}>
+        <AutoComplete
+          fullWidth={true}
+          id={this.props.id}
+          hintText={this.props.value ? this.props.value : `Prova con ${placeHolder}`}
+          dataSource={this.props.data}
+          filter={AutoComplete.caseInsensitiveFilter}
+          onNewRequest={this.props.onSelect} />
+      </div>
     );
   }
 }
