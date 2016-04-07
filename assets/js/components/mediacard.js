b69c8adc437ac4ca9d08949c83fdb48c45bd6c3f
Marco Cedaro <cedmax@cedmachine.local>
2016-04-07 21:01:24 +0100

    fix video positioning

--- assets/js/components/mediacard.js
+++ assets/js/components/mediacard.js
@@ -39,7 +39,7 @@ export default class MediaCard extends React.Component {
 
     let styles = {
       position: 'absolute',
-      top: 'calc(30% + 200px)',
+      top: 'calc(25% + 200px)',
       left: '50%',
       transform: 'translate3D(-50%,-50%,0)',
       width: '80vmin'
